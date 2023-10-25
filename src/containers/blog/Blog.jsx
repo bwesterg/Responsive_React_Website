622c89b82900a00b231e6d740ebe696c9402a6b3
bwesterg <benton.westergaard@gmail.com>
2023-10-25 14:37:06 -0600

    adds all sections and styling minus footer.

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Article } from '../../components';
+import './blog.css';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 function Blog() {
     return (
-        <div>
-            Blog
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">A lot is happening, and we are blogging on the subject</h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-container_groupA">
+                    <Article imgUrl={blog01} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
+                </div>
+                <div className="gpt3__blog-container_groupB">
+                    <Article imgUrl={blog02} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
+                    <Article imgUrl={blog03} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
+                    <Article imgUrl={blog04} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
+                    <Article imgUrl={blog05} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/> 
+                </div>
+            </div>
         </div>
     )
 }
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './possibility.css';
 
+import possibilityImage from '../../assets/possibility.png'
+
 function Possibility() {
     return (
-        <div>
-            Possibility
+        <div className="gpt3__possibility section__padding" id="possibility">
+            <div className="gpt3__possibility-image">
+                <img src={possibilityImage} alt="possibilities image"/>
+            </div>
+            <div className="gpt3__possibility-content">
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className="gradient__text">A Land of Pure Imagination</h1>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                <h4>You should never, never, doubt what nobody is sure about</h4>
+            </div>
         </div>
     )
 }
