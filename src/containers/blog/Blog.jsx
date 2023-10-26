1558e8588c47129f93ae9e012f8de7cdfcc8be5f
bwesterg <benton.westergaard@gmail.com>
2023-10-26 16:17:03 -0600

    finishes footer plus some assorted site wide style updates.

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,17 +7,17 @@ function Blog() {
     return (
         <div className="gpt3__blog section__padding" id="blog">
             <div className="gpt3__blog-heading">
-                <h1 className="gradient__text">A lot is happening, and we are blogging on the subject</h1>
+                <h1 className="gradient__text">Our engineers are excited for the future</h1>
             </div>
             <div className="gpt3__blog-container">
                 <div className="gpt3__blog-container_groupA">
                     <Article imgUrl={blog01} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
                 </div>
                 <div className="gpt3__blog-container_groupB">
-                    <Article imgUrl={blog02} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
-                    <Article imgUrl={blog03} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
-                    <Article imgUrl={blog04} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/>
-                    <Article imgUrl={blog05} date="October 25, 2023" title="Open AI is the future. Let us explore the possibilities"/> 
+                    <Article imgUrl={blog02} date="October 25, 2023" title="AI is open to the future. Let us possibly explore"/>
+                    <Article imgUrl={blog03} date="October 25, 2023" title="Possibly, AI is openly future. Let's now explore"/>
+                    <Article imgUrl={blog04} date="October 25, 2023" title="Explore the possibilities of an AI future"/>
+                    <Article imgUrl={blog05} date="October 25, 2023" title="Futuristic AI is possibly explorable"/> 
                 </div>
             </div>
         </div>
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,10 +6,10 @@ function WhatGPT3() {
     return (
         <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
             <div className="gpt3__whatgpt3-feature">
-                <Feature title="What is GPT-3" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae" />
+                <Feature title="What is GPT-3?" text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae" />
             </div>
             <div className="gpt3__whatgpt3-heading">
-                <h1 className="gradient__text">Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
+                <h1 className="gradient__text">We offer a variety of services to harness the power of artificial intelligence</h1>
                 <p>Explore the Library</p>
             </div>
             <div className="gpt3__whatgpt3-container">
